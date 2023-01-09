59441882c4fdbe76b95ee038f5bdf97872788bdd
unknown <kafukom2@gmail.com>
2023-01-09 13:15:46 +0300

    Added functionality for adding books to library

--- Book Library/app.js
+++ Book Library/app.js
@@ -1,8 +1,15 @@
+/* eslint-disable camelcase */
 /* eslint-disable no-restricted-syntax */
 /* eslint-disable prefer-const */
 /* eslint-disable guard-for-in */
 const content = document.querySelector('.content');
-const btn = document.querySelector('.btn-primary');
+const submit = document.querySelector('.submit');
+const addBook = document.querySelector('.addBook');
+const form = document.querySelector('.form');
+const form_title = document.querySelector('input[name="book_title"]');
+const form_author = document.querySelector('input[name="book_author"]')
+const form_pages = document.querySelector('input[name="book_pages"]')
+const form_status = document.querySelector('input[name="status"]')
 
 
 const myLibrary = [];
@@ -13,8 +20,9 @@ function Book(title, author, pages, status) {
     this.pages = pages;
     this.status = status;
 };
-const book1 = new Book('alex rider', 'anthony horowitz', 300, 'read');
-const book2 = new Book('cherub', 'robert muchamore', 400, 'not read');
+
+// const book1 = new Book('alex rider', 'anthony horowitz', 300, 'read');
+// const book2 = new Book('cherub', 'robert muchamore', 400, 'not read');
 
 
 
@@ -26,26 +34,37 @@ function addBookToLibrary(bookObj) {
 
 
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
 
+form.style.display = 'none';
+
+addBook.addEventListener('click', () => {
+    form.style.display = 'block';
+    form.reset();
+
+});
 
 
 
 
 console.log(myLibrary);
-btn.addEventListener('click', () => {
-    for (let book in myLibrary) {
-        let bodyContent = document.createElement('div');
-        bodyContent.innerHTML = `<h4>${myLibrary[book].title}</h4>
+submit.addEventListener('click', (e) => {
+    e.preventDefault();
+    console.log(form_title.value)
+    const new_book = new Book(form_title.value, form_author.value, form_pages.value, form_status.value)
+    addBookToLibrary(new_book);
+    form.style.display = 'none';
+    let bodyContent = document.createElement('div');
+    bodyContent.innerHTML = `<h4>${new_book.title}</h4>
                                         by
-                                    <div>${myLibrary[book].author}</div>
-                                    <div>Pages: ${myLibrary[book].pages}</div>
-                                    <div>Status: ${myLibrary[book].status}</div>`
+                                    <div>${new_book.author}</div>
+                                    <div>Pages: ${new_book.pages}</div>
+                                    <div>Status: ${new_book.status}</div>`
 
-        bodyContent.setAttribute('class', 'card');
-        content.appendChild(bodyContent);
+    bodyContent.setAttribute('class', 'card');
+    content.appendChild(bodyContent);
 
-    }
+
 
 });
--- README.md
+++ README.md
@@ -1,9 +1,11 @@
 # odin-projects
+
 Repository containing all project assignments from the ODIN PROJECT website.
+
 1. [Etch A Sketch](https://kafukom.github.io/odin-projects/etch-a-sketch/index.html)
 2. [Rock, Paper & Scissors](https://kafukom.github.io/odin-projects/rock-paper-scissors/rock_paper_scissors.html)
 3. [Simple Landing Page](https://kafukom.github.io/odin-projects/Simple-Landing-Page/index.html)
 4. [Calculator](https://kafukom.github.io/odin-projects/calculator/index.html)
 5. [Sign Up Form](https://kafukom.github.io/odin-projects/Sign%20up%20Form/index.html)
 6. [Admin Dashboard](https://kafukom.github.io/odin-projects/Dashboard/index.html)
-
+7. [Book Library](https://github.com/kafukoM/odin-projects/blob/main/Book%20Library/index.html)
