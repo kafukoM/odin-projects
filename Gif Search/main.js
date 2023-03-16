8eb56ad548164f32d9caf32be1faa82f5186afd5
unknown <kafukom2@gmail.com>
2023-03-16 16:00:51 +0300

    Added async/await functionality

--- Gif Search/main.js
+++ Gif Search/main.js
@@ -9,9 +9,33 @@ let image = document.createElement('img');
 
 
 
-btn.addEventListener('click', () => {
-    let search = document.querySelector('#gifName').value;
+btn.addEventListener('click', getImage);
 
+//USING ASYNC/AWAIT
+async function getImage() {
+    let search = document.querySelector('#gifName').value;
+    try {
+        let response =
+            await fetch(`https://api.giphy.com/v1/gifs/translate?api_key=meX0Mr0eqPejNKslHigvWVTQz81fmVSX&s=${search}`, {
+                mode: 'cors'
+            })
+        let imageData = await response.json()
+        image.src = ''
+
+        image.src = imageData.data.images.original.url
+
+        container.appendChild(image)
+
+    } catch (error) {
+        console.log(error)
+    }
+
+
+
+
+}
+
+/* USING PROMISES
     fetch(`https://api.giphy.com/v1/gifs/translate?api_key=meX0Mr0eqPejNKslHigvWVTQz81fmVSX&s=${search}`, {
             mode: 'cors'
         })
@@ -27,4 +51,4 @@ btn.addEventListener('click', () => {
             container.appendChild(image)
         })
         .catch(err => { console.err('Error in image retrieval ' + err) })
-})
+        */
